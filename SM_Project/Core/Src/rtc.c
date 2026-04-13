b8e2526019a35ca0e29196244de9b211427b8179
PuyGonzalo <gonza.puy@gmail.com>
2026-04-13 00:03:18 -0300

    Preservo hora del RTC entre resets usando backup register
    Se usa BKP_DR0 como flag para detectar si el RTC ya fue inicializado.
    En resets posteriores se saltea el seteo de fecha/hora default,
    preservando la hora sincronizada con el HES.

--- SM_Project/Core/Src/rtc.c
+++ SM_Project/Core/Src/rtc.c
@@ -127,7 +127,9 @@ void MX_RTC_Init(void)
   }
 
   /* USER CODE BEGIN Check_RTC_BKUP */
-
+  if (HAL_RTCEx_BKUPRead(&hrtc, RTC_BKP_DR0) == 0xBEBE) {
+    return;
+  }
   /* USER CODE END Check_RTC_BKUP */
 
   /** Initialize RTC and set the Time and Date
@@ -151,7 +153,7 @@ void MX_RTC_Init(void)
     Error_Handler();
   }
   /* USER CODE BEGIN RTC_Init 2 */
-
+  HAL_RTCEx_BKUPWrite(&hrtc, RTC_BKP_DR0, 0xBEBE);
   /* USER CODE END RTC_Init 2 */
 
 }
