23aa51965f0ef96683c1fc8a52e092b4d3025dfd
PuyGonzalo <gonza.puy@gmail.com>
2026-05-31 02:25:02 -0300

    feat(LPM): PA8 NOPULL + IT_FALLING para pull-up externo (elimina fuga en STOP)

--- SM_Project/Core/Src/gpio.c
+++ SM_Project/Core/Src/gpio.c
@@ -64,8 +64,8 @@ void MX_GPIO_Init(void)
 
   /*Configure GPIO pin : PULSE_INPUT_Pin */
   GPIO_InitStruct.Pin = PULSE_INPUT_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(PULSE_INPUT_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pin : QUECTEL_STATUS_Pin */
